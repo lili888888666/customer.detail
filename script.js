51701c1c4f9b58597b15fdccd8eadb66cbbcb9f4
kevin <kevin@au.com>
2025-05-07 19:42:42 +1000

    fix

--- script.js
+++ script.js
@@ -3,17 +3,8 @@ import { createDetailsWidget } from "@livechat/agent-app-sdk";
 createDetailsWidget().then((widget) => {
     widget.on("customer_profile", (profile) => {
         const text = `Customer Name: ${profile.name || "Unknown"}`;
+        const fullProfile = JSON.stringify(profile, null, 2); // Pretty-print
         document.getElementById("infoText").textContent = text;
-
-        // Optional: update widget title dynamically
-        widget.modifySection({ title: "Customized Customer Info",
-        components: [
-            {
-                type: "Customized Customer Name",
-                data: {
-                    value: text
-                }
-            }
-        ]});
+        document.getElementById("infoText2").textContent = fullProfile;
     });
 });
